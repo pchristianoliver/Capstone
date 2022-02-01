df2c8261f5daff665445bbfe142b2fa8433e6c7d
pchristianoliver <pchristianoliver@gmail.com>
2022-02-01 15:14:28 +0800

    Sample Commit pre eto malupet ayaw gumana

--- app/src/main/java/com/example/contacttracingapplication/MainActivity.java
+++ app/src/main/java/com/example/contacttracingapplication/MainActivity.java
@@ -2,14 +2,38 @@ package com.example.contacttracingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button registerBtn;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerBtn.findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
+        }
     }
-}
+
+
+
 //comment
