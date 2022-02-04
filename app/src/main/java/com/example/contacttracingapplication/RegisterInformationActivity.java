aafbe12da070981868f8f7f5c0d553a0f7b90775
pchristianoliver <pchristianoliver@gmail.com>
2022-02-04 14:38:21 +0800

    ang lupet

--- app/src/main/java/com/example/contacttracingapplication/RegisterInformationActivity.java
+++ app/src/main/java/com/example/contacttracingapplication/RegisterInformationActivity.java
@@ -2,13 +2,100 @@ package com.example.contacttracingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class RegisterInformationActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
+
+public class RegisterInformationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] gender = {"Male","Female"};
+    private EditText firstName, lastName, middleName, birthdate;
+    DatePickerDialog.OnDateSetListener setListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_information);
+
+        firstName = findViewById(R.id.firstname);
+        lastName = findViewById(R.id.lastname);
+        middleName = findViewById(R.id.middlename);
+        birthdate = findViewById(R.id.birthdate);
+
+
+        //this if for gender
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, gender);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+
+        //this is for birthdate
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        birthdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        RegisterInformationActivity.this, android.R.style.Theme_Translucent, setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+                datePickerDialog.show();
+
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month=month+1;
+                String date = day+"/"+month+"/"+year;
+                birthdate.setText(date);
+
+            }
+        };
+
+        birthdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(RegisterInformationActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month=month+1;
+                        String date = day+"/"+month+"/"+year;
+                        birthdate.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), "Selected User: "+gender[position] , Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
