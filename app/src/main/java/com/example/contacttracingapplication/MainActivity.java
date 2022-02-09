5a19931e1a949a6488a3b61b54e874cc90078a48
pchristianoliver <pchristianoliver@gmail.com>
2022-02-09 17:32:05 +0800

    ui updated

--- app/src/main/java/com/example/contacttracingapplication/MainActivity.java
+++ app/src/main/java/com/example/contacttracingapplication/MainActivity.java
@@ -33,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
