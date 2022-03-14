444a1ead14c9d1a3a36e025b879a4a5dffb4c928
pchristianoliver <pchristianoliver@gmail.com>
2022-03-14 15:28:35 +0800

    symptom failed

---
+++ app/src/main/java/com/example/contacttracingapplication/SymptomActivity.java
@@ -0,0 +1,14 @@
+package com.example.contacttracingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SymptomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symptom);
+    }
+}
