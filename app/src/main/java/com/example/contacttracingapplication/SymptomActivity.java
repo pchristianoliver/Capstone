668e7541047dbdd7fefb7fb8dc69ecfe4640ec68
pchristianoliver <pchristianoliver@gmail.com>
2022-03-18 13:35:43 +0800

    error

--- app/src/main/java/com/example/contacttracingapplication/SymptomActivity.java
+++ app/src/main/java/com/example/contacttracingapplication/SymptomActivity.java
@@ -1,6 +1,8 @@
 package com.example.contacttracingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -16,6 +18,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.contacttracingapplication.adapters.SymptomAdapter;
 import com.example.contacttracingapplication.ui.home.HomeFragment;
 
 import org.json.JSONException;
@@ -26,12 +29,20 @@ import java.util.List;
 
 public class SymptomActivity extends AppCompatActivity {
     Button submit_button, cancel_btn;
-    CheckBox fever_checkbox, dryCough_checkbox, fatigue_checkbox, aches_checkbox,
-            runnyNose_checkbox, soreThroat_checkbox, diarrhea_checkbox,
-            headache_checkbox, lostOfSmell_checkbox;
+    CheckBox fever_checkbox;
     List<String> userSymptoms = new ArrayList<String>();
     String userId = "", userHealthStatusId = "";
     SharedPreferences storedData;
+    RecyclerView symptoms_recyclerView;
+
+    String[] title = {
+            "Abundance",
+            "Anxiety",
+            "Bruxism",
+            "Discipline",
+            "Drug Addiction"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,18 +50,12 @@ public class SymptomActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_symptom);
 
+        symptoms_recyclerView = findViewById(R.id.symptomsRecycleView);
         submit_button = findViewById(R.id.submitBtn);
         cancel_btn = findViewById(R.id.cancel_btn);
 
         fever_checkbox = findViewById(R.id.feverCheckbox);
-        dryCough_checkbox = findViewById(R.id.dryCoughCheckbox);
-        fatigue_checkbox = findViewById(R.id.fatigueCheckbox);
-        aches_checkbox = findViewById(R.id.acheCheckbox);
-        runnyNose_checkbox = findViewById(R.id.runnyNoseCheckbox);
-        soreThroat_checkbox = findViewById(R.id.soreThroatCheckbox);
-        diarrhea_checkbox = findViewById(R.id.diarrheaCheckbox);
-        headache_checkbox = findViewById(R.id.headAcheCheckbox);
-        lostOfSmell_checkbox = findViewById(R.id.lostOfSmellCheckbox);
+
 
         storedData = getSharedPreferences("storedData", Context.MODE_PRIVATE);
         userId = storedData.getString("userId", "");
@@ -59,7 +64,7 @@ public class SymptomActivity extends AppCompatActivity {
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (String symptom: userSymptoms) {
+                for (String symptom : userSymptoms) {
                     SaveUserSymptoms(symptom);
                 }
             }
@@ -72,16 +77,21 @@ public class SymptomActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        SymptomAdapter adapter = new SymptomAdapter(title);
+        symptoms_recyclerView.setAdapter(adapter);
+        symptoms_recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public void SymptomCheck(View v) {
-        CheckBox checkBox = (CheckBox)v;
-        if(checkBox.isChecked()){
+        CheckBox checkBox = (CheckBox) v;
+        if (checkBox.isChecked()) {
             userSymptoms.add(checkBox.getText().toString());
         }
     }
 
     private String API_URL = "https://mclogapi20220308122258.azurewebsites.net/api/";
+
     public void SaveUserSymptoms(String symptom) {
         JSONObject userObject = new JSONObject();
         try {
---
+++ app/src/main/java/com/example/contacttracingapplication/adapters/SymptomAdapter.java
@@ -0,0 +1,52 @@
+package com.example.contacttracingapplication.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contacttracingapplication.R;
+
+
+public class SymptomAdapter extends RecyclerView.Adapter<SymptomAdapter.ViewHolder> {
+
+    private String[] localDataSet;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_symptoms, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    public SymptomAdapter(String[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.fever.setText(localDataSet[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private CheckBox fever;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            fever = itemView.findViewById(R.id.feverCheckbox);
+
+
+        }
+    }
+}
