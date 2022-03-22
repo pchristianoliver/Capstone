b55b09823fb515f92b397facd6bfa2e3178dab8d
Allain <itsallain@gmail.com>
2022-03-22 22:36:01 +0800

    testing_v1

--- app/src/main/java/com/example/contacttracingapplication/MainActivity.java
+++ app/src/main/java/com/example/contacttracingapplication/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.contacttracingapplication;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,38 +7,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.FirebaseTooManyRequestsException;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
-import com.google.firebase.auth.PhoneAuthProvider;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -48,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText etPassword, etUsername;
     private TextView txtRegister;
+    ProgressBar progressBar;
     SharedPreferences storedData;
 
     @Override
@@ -61,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.editTextTextPassword);
         txtRegister = findViewById(R.id.txtRegisterHere);
         storedData = getSharedPreferences("storedData", Context.MODE_PRIVATE);
+        progressBar = findViewById(R.id.progress_bar);
 
         txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,40 +53,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
                 AuthenticateUser();
             }
         });
     }
 
-    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        FirebaseAuth.getInstance().signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("signInSuccess", "");
-
-                            FirebaseUser user = task.getResult().getUser();
-                            // Update UI
-                        } else {
-                            // Sign in failed, display a message and update the UI
-                            Log.w("signInfailure", task.getException());
-                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                // The verification code entered was invalid
-                            }
-                        }
-                    }
-                });
-    }
-
+    final String API_URL = "https://mclogapi20220308122258.azurewebsites.net/api/";
     public void AuthenticateUser() {
         JSONObject credentialObject = new JSONObject();
-        String API_URL = "https://mclogapi20220308122258.azurewebsites.net/api/Authentication";
         try {
             credentialObject.put("mobileNumber", etUsername.getText().toString());
             credentialObject.put("password", etPassword.getText().toString());
@@ -113,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.POST,
-                API_URL,
+                API_URL + "Authentication",
                 credentialObject,
                 response -> {
                     try {
@@ -122,7 +85,11 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 },
-                error -> Log.e("Rest_Response", error.toString())
+                error -> {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    Log.e("Rest_Response", error.toString());
+                }
         );
 
         jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
@@ -133,73 +100,15 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
     }
 
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-        @Override
-        public void onVerificationCompleted(PhoneAuthCredential credential) {
-            // This callback will be invoked in two situations:
-            // 1 - Instant verification. In some cases the phone number can be instantly
-            //     verified without needing to send or enter a verification code.
-            // 2 - Auto-retrieval. On some devices Google Play services can automatically
-            //     detect the incoming verification SMS and perform verification without
-            //     user action.
-            signInWithPhoneAuthCredential(credential);
-            Log.d(TAG, "onVerificationCompleted:" + credential);
-        }
-
-        @Override
-        public void onVerificationFailed(FirebaseException e) {
-            // This callback is invoked in an invalid request for verification is made,
-            // for instance if the the phone number format is not valid.
-            Log.w(TAG, "onVerificationFailed", e);
-
-            if (e instanceof FirebaseAuthInvalidCredentialsException) {
-                // Invalid request
-                Log.e("onVerificationFailed: ", "Invalid request");
-            } else if (e instanceof FirebaseTooManyRequestsException) {
-                // The SMS quota for the project has been exceeded
-                Log.e("onVerificationFailed: ", "Quota");
-            }
-
-            // Show a message and update the UI
-        }
-
-        @Override
-        public void onCodeSent(@NonNull String verificationId,
-                               @NonNull PhoneAuthProvider.ForceResendingToken token) {
-            // The SMS verification code has been sent to the provided phone number, we
-            // now need to ask the user to enter the code and then construct a credential
-            // by combining the code with a verification ID.
-            Log.d(TAG, "onCodeSent:" + verificationId);
-
-            // Save verification ID and resending token so we can use them late
-            //mResendToken = token;
-        }
-    };
-
-    public void StartValidatePhoneNumber() {
-        PhoneAuthOptions option = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
-                .setPhoneNumber("+639216286417")
-                .setTimeout(60L, TimeUnit.SECONDS)
-                .setActivity(this)
-                .setCallbacks(mCallbacks)
-                .build();
-        PhoneAuthProvider.verifyPhoneNumber(option);
-    }
-
     private void RedirectToMainPage(@NonNull String message) {
         if (message.length() > 5) {
             Toast.makeText(getApplicationContext(), "Incorrect username or password", Toast.LENGTH_SHORT).show();
             return;
         } else {
             Intent intent = new Intent(MainActivity.this, MainActivityWithNavigation.class);
-            // ===================================================================
-            /*
-                SAVE DATA WHICH ALLOWS ACTIVITIES TO ACCESS AND REUSE VALUES
-            */
             SharedPreferences.Editor editor = storedData.edit();
             editor.putString("userId", message);
             editor.commit();
-            // ===================================================================
             startActivity(intent);
             finishAfterTransition();
         }
